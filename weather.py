4bca63d71486dcd2565a4de7d21248d05b750339
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2019-08-02 17:21:30 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import pyowm
 #https://github.com/csparpa/pyowm
 city = input("Ваш город:    ")
-owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language='ru')
+owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language='ru') #Your language
 observation = owm.weather_at_place(city)
 
 w = observation.get_weather()
@@ -9,7 +9,7 @@ temperature=w.get_temperature('celsius')['temp']
 humidity=w.get_humidity()
 speedWind=w.get_wind()["speed"]
 
-print("В " + city +"е сейчас " + w.get_detailed_status())
-print("Сейчас температура: " + str(temperature) + " по Цельсию.")
-print("Влажность = "+ str(humidity))
-print('Скорость ветра = '+str(speedWind))
+print("В " + city +"е сейчас " + w.get_detailed_status()) #weather
+print("Сейчас температура: " + str(temperature) + " по Цельсию.")  #temperature
+print("Влажность = "+ str(humidity)) #humidity
+print('Скорость ветра = '+str(speedWind)) #speed wind
